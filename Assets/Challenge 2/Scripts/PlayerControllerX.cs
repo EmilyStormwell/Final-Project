5d4da6823ee437b9c8c0452c5c25e52a341013f2
EmilyStormwell <ecmcmurry25@gmail.com>
2025-11-13 13:02:25 -0500

    Project Begun!

---
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private float nextValidTime = 0f;
+    private float cooldownTime = 3.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time >= nextValidTime)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+
+            nextValidTime = Time.time + cooldownTime;
+        }
+    }
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,47 @@
+using TMPro;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float horizontalInput;
+    public float speed = 10.0f;
+    private float xRange = 25;
+    public GameObject projectilePrefab;
+    public int ammo = 0;
+    public float ammoDelay = 1;
+    public float ammoInterval = 1;
+    public TextMeshProUGUI ammoText;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        InvokeRepeating("IncreaseAmmo", ammoDelay, ammoInterval);
+        ammoText.text = "Ammo: " + ammo;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalInput * speed * Time.deltaTime);
+
+        if (transform.position.x < -xRange)
+        {
+            transform.position = new Vector3 (-xRange, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+        if (Input.GetKeyDown(KeyCode.Space) && ammo > 0)
+        {
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+            ammo--;
+            ammoText.text = "Ammo: " + ammo;
+        }
+    }
+    void IncreaseAmmo()
+    {
+        ammo++;
+        ammoText.text = "Ammo: " + ammo;
+    }
+}
